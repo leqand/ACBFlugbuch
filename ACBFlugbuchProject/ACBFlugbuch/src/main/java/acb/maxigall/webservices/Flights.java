2cc544df26e5546e091ed54e91c76e376c38db65
leqand <maxi.gall12@googlemail.com>
2014-03-31 17:25:21 +0200

    Added DatePickerDialog.

--- ACBFlugbuchProject/ACBFlugbuch/src/main/java/acb/maxigall/webservices/Flights.java
+++ ACBFlugbuchProject/ACBFlugbuch/src/main/java/acb/maxigall/webservices/Flights.java
@@ -106,6 +106,12 @@ public class Flights extends Activity {
                 "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"de-DE\" lang=\"de-DE\">" +
                 "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" /> <head></head><body> <h1> ACBFlugbuch </h1> <br> (c) 2013-2014 Maximilian Gall. </body></html>","text/html; charset=UTF-8" ,null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(v)
+                .setTitle("FlugZusammenfassung");
+        AlertDialog mAlertDialog = builder.create();
+        mAlertDialog.show();
     }
 
     public void setup(){
